83903a6e1e2ed1d295403600f50ac6d774561c55
shushu0207 <zhiyuchen0207@gmail.com>
2025-12-19 17:33:10 +0900

    added tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", // Reactコンポーネントを監視対象にする
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
