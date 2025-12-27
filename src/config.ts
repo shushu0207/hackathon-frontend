29df46e4b697f354ad916e55f0c28275e4599c65
shushu0207 <zhiyuchen0207@gmail.com>
2025-12-28 00:12:51 +0900

    a

--- src/config.ts
+++ src/config.ts
@@ -1,1 +1,3 @@
-export const API_BASE_URL = process.env.REACT_APP_API_URL || "http://localhost:8080";
+export const API_BASE_URL = "https://hackathon-backend-7301681102.us-central1.run.app";
+
+// export const API_BASE_URL = process.env.REACT_APP_API_URL || "http://localhost:8080";
